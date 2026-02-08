336f922cd2e42d6781e129d31bd11fb7ae86b487
gulnar-hqvrd <gulnarhagverdiyeva18@gmail.com>
2026-02-08 20:48:54 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Students from './Students.jsx'
+
+function App() {
+  return (
+    <div>
+      
+      <h1 className='headers'>Hello, React!</h1>
+      <Students />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Students.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Students() {
+  return (
+    <div>
+      <h2>Students Page</h2>
+      <ul> 
+        <li>Student 1</li>
+        <li>Student 2</li>
+        <li>Student 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Students
